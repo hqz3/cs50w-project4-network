68f77fe9c7e46b926fbbc1a3f1250d7084e2a93c
ZHQ <herman.zheng@aya.yale.edu>
2021-09-16 23:59:00 -0400

    Prettier formatting

--- network/static/network/profile.js
+++ network/static/network/profile.js
@@ -1,53 +1,67 @@
-import { likeButtons, handleLikeClick, editButtons, handleEditClick } from './script.js';
-likeButtons.forEach(button => button.addEventListener('click', handleLikeClick));
-if (editButtons.length) editButtons.forEach(button => {
-    button.addEventListener('click', handleEditClick);
-})
-
+import {
+  likeButtons,
+  handleLikeClick,
+  editButtons,
+  handleEditClick,
+} from "./script.js";
+likeButtons.forEach((button) =>
+  button.addEventListener("click", handleLikeClick)
+);
+if (editButtons.length)
+  editButtons.forEach((button) => {
+    button.addEventListener("click", handleEditClick);
+  });
 
 // Update following/followers count when toggling "Follow" button
 const followerCount = document.querySelector(".follower-count");
 const toggleFollowButton = document.querySelector(".toggle-follow-button");
 
 if (toggleFollowButton) {
-    toggleFollowButton.addEventListener('click', handleToggleFollowClick);
-    
-    if (toggleFollowButton.id === "unfollow") {
-        toggleFollowButton.addEventListener('mouseenter', handleUnfollowMouseOver);
-        toggleFollowButton.addEventListener('mouseleave', handleUnfollowMouseLeave);
-    }
+  toggleFollowButton.addEventListener("click", handleToggleFollowClick);
+
+  if (toggleFollowButton.id === "unfollow") {
+    toggleFollowButton.addEventListener("mouseenter", handleUnfollowMouseOver);
+    toggleFollowButton.addEventListener("mouseleave", handleUnfollowMouseLeave);
+  }
 }
 
 function handleToggleFollowClick(e) {
-    let username = e.currentTarget.getAttribute('value');
-    fetch(`/profile/${username}/toggle_follow`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.message);
-        // Update "Followers" count
-        followerCount.textContent = data.viewed_user_followers;
-    })
-    
-    let action = e.currentTarget.getAttribute('id');
-    toggleFollowButton.setAttribute('id', action === "follow" ? "unfollow" : "follow");
-    action = e.currentTarget.getAttribute('id');
-    toggleFollowButton.textContent = action === "follow" ? "Follow" : "Following";
+  let username = e.currentTarget.getAttribute("value");
+  fetch(`/profile/${username}/toggle_follow`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.message);
+      // Update "Followers" count
+      followerCount.textContent = data.viewed_user_followers;
+    });
 
-    if (action === "unfollow") {
-        toggleFollowButton.addEventListener('mouseenter', handleUnfollowMouseOver);
-        toggleFollowButton.addEventListener('mouseleave', handleUnfollowMouseLeave);
-    }
-    else {
-        toggleFollowButton.removeEventListener('mouseenter', handleUnfollowMouseOver);
-        toggleFollowButton.removeEventListener('mouseleave', handleUnfollowMouseLeave);    
-    }
-    
+  let action = e.currentTarget.getAttribute("id");
+  toggleFollowButton.setAttribute(
+    "id",
+    action === "follow" ? "unfollow" : "follow"
+  );
+  action = e.currentTarget.getAttribute("id");
+  toggleFollowButton.textContent = action === "follow" ? "Follow" : "Following";
+
+  if (action === "unfollow") {
+    toggleFollowButton.addEventListener("mouseenter", handleUnfollowMouseOver);
+    toggleFollowButton.addEventListener("mouseleave", handleUnfollowMouseLeave);
+  } else {
+    toggleFollowButton.removeEventListener(
+      "mouseenter",
+      handleUnfollowMouseOver
+    );
+    toggleFollowButton.removeEventListener(
+      "mouseleave",
+      handleUnfollowMouseLeave
+    );
+  }
 }
 
 function handleUnfollowMouseOver(e) {
-    toggleFollowButton.textContent = "Unfollow";
+  toggleFollowButton.textContent = "Unfollow";
 }
 
 function handleUnfollowMouseLeave(e) {
-    toggleFollowButton.textContent = "Following";
+  toggleFollowButton.textContent = "Following";
 }
--- network/static/network/script.js
+++ network/static/network/script.js
@@ -1,96 +1,94 @@
 const postBody = document.querySelector(".new-post-form textarea");
 const postButton = document.querySelector(".submit-container input");
-if (postBody) postBody.addEventListener('keyup', handlePostBodyInput);
+if (postBody) postBody.addEventListener("keyup", handlePostBodyInput);
 
 // Post submit button enabled after user inputs text
 function handlePostBodyInput(e) {
-    if (postBody.value === "") {
-        postButton.disabled = true;
-        postButton.style = "filter: opacity(50%)"
-    }
-    else {
-        postButton.style = "filter: opacity(100%)"
-        postButton.disabled = false;
-    } 
+  if (postBody.value === "") {
+    postButton.disabled = true;
+    postButton.style = "filter: opacity(50%)";
+  } else {
+    postButton.style = "filter: opacity(100%)";
+    postButton.disabled = false;
+  }
 }
-
 
 // Toggling edit buttons
 export const editButtons = document.querySelectorAll(".edit");
-if (editButtons.length) editButtons.forEach(button => {
-    button.addEventListener('click', handleEditClick);
-})
+if (editButtons.length)
+  editButtons.forEach((button) => {
+    button.addEventListener("click", handleEditClick);
+  });
 
 export async function handleEditClick(e) {
-    let postID = e.currentTarget.dataset.postId;
-    let postBody = document.querySelector(`div[data-post-id="${postID}"]`);
-    let originalPost = postBody.querySelector('.original-post');
-    let editForm = postBody.querySelector('.edit-form');
-    let saveButton = editForm.querySelector('.save-button');
-    if (postBody.dataset.status === 'close') {
-        originalPost.classList.add('display-hide');
-        editForm.classList.remove('display-hide');
-        postBody.dataset.status = 'open';
-        // Add eventListener for "Save button"
-        saveButton.addEventListener('click', handleSaveClick)
-    }
-    else {
-        originalPost.classList.remove('display-hide');
-        editForm.classList.add('display-hide');
-        // Reset the edit form's textarea
-        editForm.querySelector("textarea").value = originalPost.textContent;
-        postBody.dataset.status = 'close';
-        // Remove eventListener for "Save button"
-        saveButton.removeEventListener('click', handleSaveClick)
-    }
+  let postID = e.currentTarget.dataset.postId;
+  let postBody = document.querySelector(`div[data-post-id="${postID}"]`);
+  let originalPost = postBody.querySelector(".original-post");
+  let editForm = postBody.querySelector(".edit-form");
+  let saveButton = editForm.querySelector(".save-button");
+  if (postBody.dataset.status === "close") {
+    originalPost.classList.add("display-hide");
+    editForm.classList.remove("display-hide");
+    postBody.dataset.status = "open";
+    // Add eventListener for "Save button"
+    saveButton.addEventListener("click", handleSaveClick);
+  } else {
+    originalPost.classList.remove("display-hide");
+    editForm.classList.add("display-hide");
+    // Reset the edit form's textarea
+    editForm.querySelector("textarea").value = originalPost.textContent;
+    postBody.dataset.status = "close";
+    // Remove eventListener for "Save button"
+    saveButton.removeEventListener("click", handleSaveClick);
+  }
 }
 
 // Save edited post into database
 async function handleSaveClick(e) {
-    e.preventDefault();
-    let postID = (e.currentTarget.dataset.postId)
-    let postBody = document.querySelector(`div[data-post-id="${postID}"]`);
-    let content = postBody.querySelector('textarea').value;
+  e.preventDefault();
+  let postID = e.currentTarget.dataset.postId;
+  let postBody = document.querySelector(`div[data-post-id="${postID}"]`);
+  let content = postBody.querySelector("textarea").value;
 
-    let response = await fetch(`/edit-post/${postID}`, {
-        method: "PUT",
-        body: JSON.stringify({"content": content}),
-    });
-    let data = await response.json();
+  let response = await fetch(`/edit-post/${postID}`, {
+    method: "PUT",
+    body: JSON.stringify({ content: content }),
+  });
+  let data = await response.json();
 
-    // Update original post shown on webpage
-    let originalPost = postBody.querySelector('.original-post');
-    originalPost.textContent = data.content;
-    
-    // Hide edit form and show updated post
-    let editForm = postBody.querySelector('.edit-form');
-    editForm.classList.add('display-hide');
-    originalPost.classList.remove('display-hide');
-    postBody.dataset.status = 'close';
+  // Update original post shown on webpage
+  let originalPost = postBody.querySelector(".original-post");
+  originalPost.textContent = data.content;
+
+  // Hide edit form and show updated post
+  let editForm = postBody.querySelector(".edit-form");
+  editForm.classList.add("display-hide");
+  originalPost.classList.remove("display-hide");
+  postBody.dataset.status = "close";
 }
- 
 
 // When user "likes" a post, update its "Like" count in database
 export const likeButtons = document.querySelectorAll(".like-container");
-likeButtons.forEach(button => button.addEventListener('click', handleLikeClick));
+likeButtons.forEach((button) =>
+  button.addEventListener("click", handleLikeClick)
+);
 
 export function handleLikeClick(e) {
-    let postID = e.currentTarget.getAttribute('value');
-    fetch(`/post/${postID}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.message);
-            let likeContainer = document.querySelector(`[value="${postID}"]`);
-            let likeCount = likeContainer.querySelector(".like-count");
-            likeCount.innerHTML = data.count;
+  let postID = e.currentTarget.getAttribute("value");
+  fetch(`/post/${postID}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.message);
+      let likeContainer = document.querySelector(`[value="${postID}"]`);
+      let likeCount = likeContainer.querySelector(".like-count");
+      likeCount.innerHTML = data.count;
 
-            if (data.currently_liked) {
-                likeContainer.setAttribute("id", "liked");
-                likeContainer.title = "Unlike";
-            }
-            else {
-                likeContainer.setAttribute("id", "not-liked");
-                likeContainer.title = "Like";
-            }
-        })
+      if (data.currently_liked) {
+        likeContainer.setAttribute("id", "liked");
+        likeContainer.title = "Unlike";
+      } else {
+        likeContainer.setAttribute("id", "not-liked");
+        likeContainer.title = "Like";
+      }
+    });
 }
